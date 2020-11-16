9f2d15eab3fd041640b88ce1a729c49605d1a9f4
VincentLD <vledoledec@gmail.com>
2020-11-16 10:18:45 +0100

    Ajout des tests unitaires

---
+++ 16112020.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("16112020.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("16112020.UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("aa4422f0-e8c7-41ab-b750-0176ef41d3cb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 16112020.UnitTests/UnitTest1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace _16112020.UnitTests
+{
+    [TestClass]
+    public class Alveole
+    {
+        [TestMethod]
+        public void MethodeATester()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Colis
+    {
+        [TestMethod]
+        public void MethodeATester()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Conditionnement
+    {
+        [TestMethod]
+        public void MethodeATester()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Entrepot
+    {
+        [TestMethod]
+        public void ExtraireSecteur(int param)
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Lot
+    {
+        [TestMethod]
+        public void MethodeATester()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Palette
+    {
+        [TestMethod]
+        public void MethodeATester()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Palettier
+    {
+        [TestMethod]
+        public void MethodeATester()
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Secteur
+    {
+        [TestMethod]
+        public void ExtrairePalettier(int param)
+        {
+
+        }
+    }
+
+    [TestClass]
+    public class Zone
+    {
+        [TestMethod]
+        public void MethodeATester()
+        {
+
+        }
+    }
+}
