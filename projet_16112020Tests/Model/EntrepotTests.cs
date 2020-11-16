a2db7b22927b2da45cca9b3d33680fc568a82153
thierrysio <thierry28220@icloud.com>
2020-11-16 16:43:54 +0100

    test unitaire

---
+++ projet_16112020Tests/Model/EntrepotTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using projet_16112020.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet_16112020.Model.Tests
+{
+    [TestClass()]
+    public class EntrepotTests
+    {
+        [TestMethod()]
+        public void EntrepotTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void ExtraireSecteurTest()
+        {
+
+        }
+
+        [TestMethod()]
+        public void GetNbSecteurTest()
+        {
+
+        }
+    }
+}
