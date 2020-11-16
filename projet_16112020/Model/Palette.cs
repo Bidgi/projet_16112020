bf82fe2a55a6eb7ad80b298c24c90e9eae85cdce
Bidgi <64425135+Bidgi@users.noreply.github.com>
2020-11-16 08:51:10 +0100

    Creation des classe

---
+++ projet_16112020/Model/Palette.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projet_16112020.Model
+{
+    class Palette
+    {
+    }
+}
